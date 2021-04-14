42394c6716741c580b0a73bd11697a6a74494efb
Miszczoo <mcglowack@gmail.com>
2021-04-14 16:37:58 +0200

    Try to make this function work

--- src/CreateTaskObject.ts
+++ src/CreateTaskObject.ts
@@ -1,10 +1,15 @@
 import { GetInputValues, newValues } from "./GetInputValues.js";
 
-export interface ObjectWithData {
-  taskName: string;
-  taskDescription: string;
-  taskDate: string[];
-}
+// export interface ObjectWithData {
+//   taskName: string;
+//   taskDescription: string;
+//   taskDate: string;
+// }
+// export interface ObjectWithData {
+//   taskName: string;
+//   taskDescription: string;
+//   taskDate: string[];
+// }
 
 export class CreateTaskObject extends GetInputValues {
   submitButton: HTMLInputElement;
@@ -17,38 +22,62 @@ export class CreateTaskObject extends GetInputValues {
     )! as HTMLInputElement;
   }
 
+  // mergeDataToObject() {
+  //   this.submitButton.addEventListener("click", (event) => {
+  //     event.preventDefault();
+
+  //     let dataObject: ObjectWithData;
+  //     // dataObject = arrayWithInputsData;
+
+  //     if (
+  //       this.datePickerInput.value === "" ||
+  //       this.taskNameInput.value === "" ||
+  //       this.descriptionInput.value === ""
+  //     ) {
+  //       alert("please fill all the inputs");
+  //       return;
+  //     } else {
+  //       dataObject = {
+  //         taskName: this.taskNameInput.value,
+  //         taskDescription: this.descriptionInput.value,
+  //         taskDate: this.datePickerInput.value,
+  //       };
+
+  //       this.taskData.push(dataObject);
+
+  //       console.log("added!");
+  //       console.log(this.taskData);
+  //       console.log(...newValues.inputsValues);
+  //     }
+  //     // console.log(dataObject);
+  //   });
+  // }
+
   mergeDataToObject() {
+    console.log(...newValues.inputsValues);
     this.submitButton.addEventListener("click", (event) => {
       event.preventDefault();
 
-      let dataObject: ObjectWithData;
-      // dataObject = arrayWithInputsData;
-      
-      if (
-        this.datePickerInput.value === "" ||
-        this.taskNameInput.value === "" ||
-        this.descriptionInput.value === ""
-      ) {
-        alert("please fill all the inputs");
-        return;
-      } else {
-        dataObject = {
-          taskName: this.taskNameInput.value,
-          taskDescription: this.descriptionInput.value,
-          taskDate: newValues.inputsValues
-        };
+      let dataObject: {};
+      dataObject = {
+        taskName: this.taskNameInput.value,
+        taskDescription: this.descriptionInput.value,
+        taskDate: newValues.inputsValues,
+      };
 
-        this.taskData.push(dataObject);
-
-        console.log("added!");
-        console.log(this.taskData);
-        console.log(...newValues.inputsValues);
-      }
-      // console.log(dataObject);
+      this.taskData.push(dataObject);
+      console.log("taskData", this.taskData);
+      // for (let data of newValues.inputsValues) {
+      //   // console.log(data);
+      //   console.log(`data: ${data}`);
+      // }
+      newValues.inputsValues.forEach((el,i) => {
+        // console.log(el);
+        console.log(`data: ${el} ${i}`);
+      });
+     
     });
-    
   }
-  
 }
 
 export const newTaskObject = new CreateTaskObject();
